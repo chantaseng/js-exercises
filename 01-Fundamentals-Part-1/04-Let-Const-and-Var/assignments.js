c7ada0e9b4de1b60de4b5ee53fdd424d4cfd15ac
William Chan <chan.william.ts@gmail.com>
2023-06-27 14:37:38 -0400

    assignment const finished

---
+++ 01-Fundamentals-Part-1/04-Let-Const-and-Var/assignments.js
@@ -0,0 +1,17 @@
+const country = "Canada";
+// let continent = "North America";
+let population = 38250000;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+const isIsland = false;
+const language = "English";
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+language = "French";
