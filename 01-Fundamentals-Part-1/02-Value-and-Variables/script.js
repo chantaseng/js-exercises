b0c1b626084cfbff57be2e26525a47c10ae7308f
William Chan <chan.william.ts@gmail.com>
2023-06-27 13:44:54 -0400

    assignment completed

--- 01-Fundamentals-Part-1/02-Value-and-Variables/script.js
+++ 01-Fundamentals-Part-1/02-Value-and-Variables/script.js
@@ -3,3 +3,20 @@ console.log(40 + 8 + 23 - 10);
 
 console.log('Jonas');
 console.log(23);
+
+let firstName = 'Matilda';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415
+
+let myFirstJob = 'Welder';
+let myCurrentJob = 'Programmer';
+
+console.log(myCurrentJob);
