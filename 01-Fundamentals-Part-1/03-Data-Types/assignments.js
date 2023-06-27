a2063c864a9ff06cf649428f70fe07f868298d7c
William Chan <chan.william.ts@gmail.com>
2023-06-27 14:21:01 -0400

    assignment completed

---
+++ 01-Fundamentals-Part-1/03-Data-Types/assignments.js
@@ -0,0 +1,15 @@
+let country = "Canada";
+// let continent = "North America";
+let population = 38250000;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+let isIsland = false;
+let language = "English";
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
