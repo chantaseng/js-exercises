578752e2baf98487b75ba6986241693c319c97f4
William Chan <chan.william.ts@gmail.com>
2023-07-18 16:01:25 -0400

    Learned about function

---
+++ 02-Fundamentals-Part-2/02-Functions/script.js
@@ -0,0 +1,39 @@
+'use strict';
+// without strict mode, we dont get errors message on the webpage console
+
+/*
+let hasDriversLicense =  false;
+const passTest = true;
+
+// if (passTest) hasDriverLicense = true;
+// With strcit mode, i have an Uncaught ReferenceError: hasDriverLicense is not define in line 7
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive!');
+
+// const interface = 'Audio'; // Unexpected strict mode reserved word, because JS reserved this word for a feature that we might use in the future
+// const private = 534; // strict mode reserved those words so we cant use it as a variables
+*/
+
+// Functions
+
+function logger() {
+  console.log('My name is William');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  // console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${
+    oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
