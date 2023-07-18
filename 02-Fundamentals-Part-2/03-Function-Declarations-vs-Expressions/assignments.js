bffb848520c81ec18bc9873d6232ad5464545348
William Chan <chan.william.ts@gmail.com>
2023-07-18 17:13:39 -0400

    assignment: difference between declaration and expression

--- 02-Fundamentals-Part-2/03-Function-Declarations-vs-Expressions/assignments.js
+++ 02-Fundamentals-Part-2/03-Function-Declarations-vs-Expressions/assignments.js
@@ -1,3 +1,6 @@
+/*
+// Functions
+
 function describeCountry(country, population, capitalCity) {
   const input = `${country} has ${population} million people and its capital city is ${
     capitalCity}.`;
@@ -10,3 +13,30 @@ const france = describeCountry('France', 67.75, 'Paris');
 console.log(canada);
 console.log(madagascar);
 console.log(france);
+*/
+
+// Function declarations vs expression
+
+// Declaration
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const percCanada1 = percentageOfWorld1(380.25);
+const percMadagascar1 = percentageOfWorld1(2800.92);
+const percFrance1 = percentageOfWorld1(670.75);
+console.log(percCanada1);
+console.log(percMadagascar1);
+console.log(percFrance1);
+
+// Expression
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+}
+
+const percCanada2 = percentageOfWorld1(38.25);
+const percMadagascar2 = percentageOfWorld1(28.92);
+const percFrance2 = percentageOfWorld1(67.75);
+console.log(percCanada2);
+console.log(percMadagascar2);
+console.log(percFrance2);
