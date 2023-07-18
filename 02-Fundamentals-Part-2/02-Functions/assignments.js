3b37036e558bcef86baca5ba6b8a33e6322f155a
William Chan <chan.william.ts@gmail.com>
2023-07-18 16:13:12 -0400

    Created a function with 3 parameters and stored it in 3 dif variables

--- 02-Fundamentals-Part-2/02-Functions/assignments.js
+++ 02-Fundamentals-Part-2/02-Functions/assignments.js
@@ -1,3 +1,12 @@
 function describeCountry(country, population, capitalCity) {
-
+  const input = `${country} has ${population} million people and its capital city is ${
+    capitalCity}.`;
+  return input;
 }
+
+const canada = describeCountry('Canada', 38.25, 'Ottawa');
+const madagascar = describeCountry('Madagascar', 28.92, 'Antananarivo');
+const france = describeCountry('France', 67.75, 'Paris');
+console.log(canada);
+console.log(madagascar);
+console.log(france);
