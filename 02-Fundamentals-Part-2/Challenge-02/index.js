263065c485825f1e8af4ff8979f56fb87979164c
William Chan <chan.william.ts@gmail.com>
2023-07-20 12:16:10 -0400

    Created empty array of bills, tips and total, and used the .push method to insert element in different arrays

--- 02-Fundamentals-Part-2/Challenge-02/index.js
+++ 02-Fundamentals-Part-2/Challenge-02/index.js
@@ -97,14 +97,22 @@ checkWinner(scoreDolphins, scoreKoalas);
 
 const calcTip = function (bill) {
   if (bill > 50 && bill <= 300) {
-    return 0.15;
+    return (bill * 0.15);
   } else {
-    return 0.2;
+    return (bill * 0.2);
   }
 }
 
 console.log(calcTip(100));
 
-// const bills = function (bill) {
-//   return bill +
-// }
+const bills = [125, 555, 44];
+
+const tips = [];
+console.log(tips);
+
+tips.push((calcTip(bills[0])), calcTip(bills[1]), calcTip(bills[2]));
+console.log(tips);
+
+const total = [];
+total.push((bills[0] + tips[0]), (bills[1] + tips[1]), (bills[2] + tips[2]));
+console.log(total);
