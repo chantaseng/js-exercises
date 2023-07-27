e0f0ee781ab873a61dbe1eba8803919ea7f37de7
William Chan <chan.william.ts@gmail.com>
2023-07-27 12:11:13 -0400

    Implemented the logic of the game, if number is <,> or === to the secretNumber. Score logic also implemented and backgroundColor and .score.style.width has dynamic features if you guessed the right number

--- 05-Project-Guess-My-Number/script.js
+++ 05-Project-Guess-My-Number/script.js
@@ -12,11 +12,47 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess'));
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+// console.log(secretNumber);
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '👾 Game Over!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '👾 Game Over!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
