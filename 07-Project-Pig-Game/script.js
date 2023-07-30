6f345396344f462701f93dfea06d215c8f70bba5
William Chan <chan.william.ts@gmail.com>
2023-07-30 12:08:50 -0400

    Hold button function created. condition is if score is >= 100, current activePlayer wins, interface 'player--winner' added, 'active--player' and diceEl interface removed, else scores[activePlayer] = currentScore and we switch player

--- 07-Project-Pig-Game/script.js
+++ 07-Project-Pig-Game/script.js
@@ -21,29 +21,59 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0; // technically still player 0
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0; // If activePlayer is presently 0, we switch to 1
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-  // 1. Generate random dice
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    // 1. Generate random dice
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // 2. Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  // 3. Check for rolled 1
-  if (dice !== 1) {
-    // if not 1, add score to curretn score
-    // Refector this: currentScore = currentScore + dice;
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // if its 1 switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0; // technically still player 0
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0; // If activePlayer is presently 0, we switch to 1
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    // 3. Check for rolled 1
+    if (dice !== 1) {
+      // if not 1, add score to curretn score
+      // Refector this: currentScore = currentScore + dice;
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add currentScore to activePlayer score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. Check if the player's score is >= 100
+    if (scores[activePlayer] >= 20) {
+      // Finish the game
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('active--player');
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
   }
 });
