4ddd7b08772e60d7f7ef74b4de99054ccd78cd3a
William Chan <chan.william.ts@gmail.com>
2023-07-30 11:14:28 -0400

    Refactored the function so it will select the score element dynamically based on the activePlayer. Used ternary operator to switch activePlayer. Resetted currentScore when player are switched(both) and change of interface to know who is playing next

--- 07-Project-Pig-Game/script.js
+++ 07-Project-Pig-Game/script.js
@@ -1,10 +1,12 @@
 'use strict';
 
 // Selecting the Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const currentScore0 = document.getElementById('current--0');
-const currentScore1 = document.getElementById('current--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
@@ -16,22 +18,32 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generate random dice
   const dice = Math.trunc(Math.random() * 6) + 1;
+
   // 2. Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
   // 3. Check for rolled 1
   if (dice !== 1) {
     // if not 1, add score to curretn score
     // Refector this: currentScore = currentScore + dice;
     currentScore += dice;
-    currentScore0.textContent = currentScore; // CHANGE LATER
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // if its 1 switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0; // technically still player 0
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0; // If activePlayer is presently 0, we switch to 1
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
