9b2cab2a649bf420b29f2c1a58c0e220876eb3bd
William Chan <chan.william.ts@gmail.com>
2023-07-30 10:16:49 -0400

    first function created that shows the dice when you press on roll dice. Depending on the dice number, it will be added to your currentScore

--- 07-Project-Pig-Game/script.js
+++ 07-Project-Pig-Game/script.js
@@ -3,9 +3,35 @@
 // Selecting the Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const currentScore0 = document.getElementById('current--0');
+const currentScore1 = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generate random dice
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // if not 1, add score to curretn score
+    // Refector this: currentScore = currentScore + dice;
+    currentScore += dice;
+    currentScore0.textContent = currentScore; // CHANGE LATER
+  } else {
+    // if its 1 switch to next player
+  }
+});
