16b823f021a061d65e426df196d0f5bdbe6644ea
William Chan <chan.william.ts@gmail.com>
2023-07-31 15:58:33 -0400

    Learning about inner scope, outer scope, variable lookup, chain scope and the difference between function scope and block scope

--- 08-Javascript-Behind-the-Scenes/script.js
+++ 08-Javascript-Behind-the-Scenes/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Creating NEW varaible with the same name as outer scope's variable
+      const firstName = 'Steven';
+
+      // Reassigning outer scope's variable
+      output = 'NEW OUTPUT';
+
+      const str = `Oh, you are a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str); because const and let variable are block scope. So only available inside the block they were created
+    console.log(millenial); // Var variables are function scope so they ignore the block, because they are not block scope
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Will';
+calcAge(1994);
+// console.log(age);
+// printAge();
