91d1fc17978457711849275b58d1071e1059e5a3
William Chan <chan.william.ts@gmail.com>
2023-08-01 15:37:37 -0400

    Difference between regular function and arrow function for a method with the this kyword

--- 08-Javascript-Behind-the-Scenes/04-Regular-Functions-vs-Arrow-Functions/script.js
+++ 08-Javascript-Behind-the-Scenes/04-Regular-Functions-vs-Arrow-Functions/script.js
@@ -133,3 +133,51 @@ f();
 */
 
 // Regular functions vs arrow functions
+
+// var firstName = 'Mathilda';
+
+const will = {
+  firstName: 'Will',
+  year: 1994,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this;
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+      // console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+will.greet();
+will.calcAge();
+
+// Argument keyword
+
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+const addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
