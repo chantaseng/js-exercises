d662ed18ac96d51f4eb7e6e014982682a8db7b2b
William Chan <chan.william.ts@gmail.com>
2023-08-02 12:33:25 -0400

    Learned about destructuring arrays

--- 09-Data-Structures-Modern-Operators-and-Strings/01-Destructuring-Arrays/script.js
+++ 09-Data-Structures-Modern-Operators-and-Strings/01-Destructuring-Arrays/script.js
@@ -1,5 +1,6 @@
-'use strict';
+"use strict";
 
+/*
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -27,3 +28,59 @@ const restaurant = {
     },
   },
 };
+*/
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+
+// Old way to assign variable to element in an array
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// Destructuring array!
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(x);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// Main is now switch to secondary and secondary switched to main
+
+// Old way before destructuring
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary); -> Vegetarian Italian
+
+// Destructuring
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// console.log(restaurant.order(2, 0));
+
+// Recieve 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// Nesting destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default value
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
