6680279436cf37753a90042f9da040db246d8693
William Chan <chan.william.ts@gmail.com>
2023-08-03 12:47:07 -0400

    Learn about spread operator and how you can implement the spread operator in functions, do shallow copies or arrays ad objects

--- 09-Data-Structures-Modern-Operators-and-Strings/03-The-Spread-Operator/script.js
+++ 09-Data-Structures-Modern-Operators-and-Strings/03-The-Spread-Operator/script.js
@@ -65,7 +65,58 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}!`
+    );
+  },
 };
+
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, "Gnocci"];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables: array, string, maps, sets. Not objects
+const str = "Jonas";
+const letters = [...str, "", "S."];
+console.log(...str);
+console.log(letters);
+// console.log(`${...str} chan`); template literal does not expect spread operator
+
+// Real world example
+const ingredients = [
+  // prompt("Let's make pasta! Ingredient 1?"),
+  // prompt("Ingredient 2?"),
+  // prompt("Ingredient 3?"),
+];
+console.log(ingredients);
+
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+
+restaurant.orderPasta(...ingredients);
+
+// Objects
+const restaurantCopy = { foundedIn: 1998, ...restaurant, founded: "Will" };
+restaurantCopy.name = "Ristorante Roma";
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
 
 /*
 ////////////////////////////////////////
