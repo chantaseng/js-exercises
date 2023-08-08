5a78d15df4d290bcfbada5da2f73a52d0f1d8245
William Chan <chan.william.ts@gmail.com>
2023-08-08 17:51:32 -0400

    Extra exercise

--- 09-Data-Structures-Modern-Operators-and-Strings/18-String-Methods-Practice/script.js
+++ 09-Data-Structures-Modern-Operators-and-Strings/18-String-Methods-Practice/script.js
@@ -1,11 +1,5 @@
 'use strict';
 
-/*
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-*/
-
 const weekday = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 //Destructuring
 const [a, b, c, d, e, f, g] = weekday;
@@ -55,6 +49,25 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+// String Methods Practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(46);
+  console.log(output);
+}
 
 /*
 // Working with strings part 3
