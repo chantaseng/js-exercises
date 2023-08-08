28ecd50680439303959f77c2ed8afe081103ff8b
William Chan <chan.william.ts@gmail.com>
2023-08-08 18:49:30 -0400

    Distinction between value and reference

--- 10-A-Closer-Look-at-Functions/02-Arguments-Value-vs-Reference/script.js
+++ 10-A-Closer-Look-at-Functions/02-Arguments-Value-vs-Reference/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/*
+// Default parameters
+
 const bookings = [];
 
 const createBooking = function (
@@ -27,3 +30,38 @@ createBooking('LH123', 5);
 
 // How to skip a default parameter
 createBooking('LH123', undefined, 1000);
+*/
+
+// How passing arguments work: Value vs Reference
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Chan',
+  passport: 74839275,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 74839275) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// is the same as doing this
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
