28bdad958aad21053913ac0bbdb03ec0c17f19a7
William Chan <chan.william.ts@gmail.com>
2023-08-08 17:18:27 -0400

    Challenge 4 - Destructuring, for-of loop, with bunch of strings methods

--- 09-Data-Structures-Modern-Operators-and-Strings/Challenge-04/script.js
+++ 09-Data-Structures-Modern-Operators-and-Strings/Challenge-04/script.js
@@ -260,3 +260,108 @@ for (const [min, event] of gameEvents) {
   console.log(`[${half} HALF] ${min}: ${event}`);
 }
 */
+
+// Challenge #4
+
+/*
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to insert the elements), and conversion will happen when the button is pressed.
+
+Test data (pasted to textarea, including spaces):
+
+underscore_case
+ first_name
+Some_Variable
+  calculate_AGE
+delayed_departure
+
+Should produce this output (5 separate console.log outputs):
+
+underscoreCase    ✅
+firstName         ✅✅
+someVariable      ✅✅✅
+calculateAge      ✅✅✅✅
+delayedDeparture  ✅✅✅✅✅
+
+Hints:
+
+- Remember which character defines a new line in the textarea 😉
+- The solution only needs to work for a variable made out of 2 words, like a_b
+- Start without worrying about the ✅. Tackle that only after you have the variable
+name conversion working 😉
+- This challenge is difficult on purpose, so start watching the solution in case
+you're stuck. Then pause and continue!
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  // console.log('clicked!');
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, r] of rows.entries()) {
+    const [first, second] = r.toLowerCase().trim().split('_');
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+
+    /*
+    const rowSplit = r.toLowerCase().trim().split('_'); // ['underscore', 'case']
+    const secondWord = rowSplit[1]; // case
+    const capitalizationFirstLetter = secondWord[0].toUpperCase(); // C
+    const concatenate =
+      rowSplit[0] + capitalizationFirstLetter + secondWord.slice(1);
+    console.log(concatenate);
+    */
+
+    // for (const r of rows) {
+    //   const rowsLowerCase = r.toLowerCase().trim(); // underscore_case
+    //   const splitWord = rowsLowerCase.split('_'); // [underscore, case]
+    //   const separateWord = splitWord[1]; // case
+    //   const capitalizationWord = separateWord[0].toUpperCase(); // C
+    //   const concatenateWord =
+    //     splitWord[0] + (capitalizationWord + separateWord.slice(1));
+    //   console.log(concatenateWord);
+    // }
+  }
+});
+
+/*
+underscore_case -> underscoreCase
+ first_name
+Some_Variable
+  calculate_AGE
+delayed_departure
+
+willy_waller
+ existiential_crisis
+What_Life
+  yo_MAN
+delayed_departure
+*/
+
+/*
+// My solution
+for (const r of rows) {
+  const rowSplit = r.toLowerCase().trim().split('_'); // ['underscore', 'case']
+  const secondWord = rowSplit[1]; // case
+  const capitalizationFirstLetter = secondWord[0].toUpperCase(); // C
+  const concatenate =
+    rowSplit[0] + capitalizationFirstLetter + secondWord.slice(1);
+  console.log(concatenate);
+
+  // for (const r of rows) {
+  //   const rowsLowerCase = r.toLowerCase().trim(); // underscore_case
+  //   const splitWord = rowsLowerCase.split('_'); // [underscore, case]
+  //   const separateWord = splitWord[1]; // case
+  //   const capitalizationWord = separateWord[0].toUpperCase(); // C
+  //   const concatenateWord =
+  //     splitWord[0] + (capitalizationWord + separateWord.slice(1));
+  //   console.log(concatenateWord);
+  // }
+}
+*/
