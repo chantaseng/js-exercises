72e1b33169838b6306c7c6b12948c58b9160d0ac
William Chan <chan.william.ts@gmail.com>
2023-08-08 11:12:50 -0400

    Challenge 3 - for-of loop with spread operator

--- 09-Data-Structures-Modern-Operators-and-Strings/Challenge-03/script.js
+++ 09-Data-Structures-Modern-Operators-and-Strings/Challenge-03/script.js
@@ -206,3 +206,55 @@ for (const [team, odd] of Object.entries(game.odds)) {
 //   Lewandowski: 2
 // }
 */
+
+// Challenge #3
+
+// Let's continue with our football betting app! This time, we have a map called 'gameEvents' (see below) with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// Your tasks:
+
+// 1. Create an array 'events' of the different game events that happened(no duplicates)
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3. Compute and log the following string to the console: "An event happened,on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+console.log(
+  `An event happened,on average, every ${90 / gameEvents.size} minutes`
+);
+
+// 4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17: ⚽GOAL
+
+for (const [key, event] of gameEvents) {
+  if (key <= 45) console.log(`[FIRST HALF] ${key}: ${event}`);
+  else {
+    console.log(`[SECOND HALF] ${key}: ${event}`);
+  }
+}
+
+// Jonas solution
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
