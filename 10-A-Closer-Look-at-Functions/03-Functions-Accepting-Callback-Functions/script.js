95baf2afafbc841358c0690a50f78faf03ebdc98
William Chan <chan.william.ts@gmail.com>
2023-08-08 18:56:42 -0400

    first commit

---
+++ 10-A-Closer-Look-at-Functions/03-Functions-Accepting-Callback-Functions/script.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/*
+// Default parameters
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 3);
+createBooking('LH123', 5);
+
+// How to skip a default parameter
+createBooking('LH123', undefined, 1000);
+*/
+
+/*
+// How passing arguments work: Value vs Reference
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Chan',
+  passport: 74839275,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 74839275) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// is the same as doing this
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+*/
