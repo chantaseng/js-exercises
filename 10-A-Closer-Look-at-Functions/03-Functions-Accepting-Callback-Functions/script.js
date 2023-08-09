9d1df59ec3ef605c755d5d06c1440024a9fc633d
William Chan <chan.william.ts@gmail.com>
2023-08-09 16:21:54 -0400

    Learned that you can call a function inside of a function. Higher order function and lower lvl function

--- 10-A-Closer-Look-at-Functions/03-Functions-Accepting-Callback-Functions/script.js
+++ 10-A-Closer-Look-at-Functions/03-Functions-Accepting-Callback-Functions/script.js
@@ -67,3 +67,35 @@ const newPassport = function (person) {
 newPassport(jonas);
 checkIn(flight, jonas);
 */
+
+// Function accepting calllback functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+
+transformer('Javascript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
