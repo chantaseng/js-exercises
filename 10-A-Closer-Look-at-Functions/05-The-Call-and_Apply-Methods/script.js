e95598a8e5f044761eedf6f14b2c795f45e8e52c
William Chan <chan.william.ts@gmail.com>
2023-08-09 17:44:21 -0400

    call and apply method to manually define THIS keyword

--- 10-A-Closer-Look-at-Functions/05-The-Call-and_Apply-Methods/script.js
+++ 10-A-Closer-Look-at-Functions/05-The-Call-and_Apply-Methods/script.js
@@ -131,3 +131,54 @@ const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 
 greetArr('Yo')('man');
 */
+
+// The call and apply methods
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `{this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(423, 'William Chan');
+lufthansa.book(954, 'Matt Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does NOT work
+// book(23, 'Nick Suzuki');
+
+book.call(eurowings, 23, 'Nick Suzuki');
+console.log(eurowings);
+
+book.call(lufthansa, 253, 'Mary Poppins');
+
+const swiss = {
+  airline: 'Swiss Air lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 534, 'Mary Cooper');
+console.log(swiss);
+
+// Apply method
+const flightData = [583, 'Tommy Jeans'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+// apply no longer used, instead we use call with spread operator
+book.call(swiss, ...flightData);
