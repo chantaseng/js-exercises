983c255ea343307e1704621a5b941c9e00cb8c80
William Chan <chan.william.ts@gmail.com>
2023-08-10 14:03:00 -0400

    first commit

---
+++ 10-A-Closer-Look-at-Functions/07-Immediately-Invoked-Function-Expressions/script.js
@@ -0,0 +1,234 @@
+'use strict';
+
+/*
+// Default parameters
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 3);
+createBooking('LH123', 5);
+
+// How to skip a default parameter
+createBooking('LH123', undefined, 1000);
+*/
+
+/*
+// How passing arguments work: Value vs Reference
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Chan',
+  passport: 74839275,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 74839275) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// is the same as doing this
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+*/
+
+/*
+// Function accepting calllback functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+
+transformer('Javascript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+*/
+
+/*
+// Functions returning functions
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+const greet1 = greeting => {
+  return name => {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+greet1('Hi')('Will');
+
+// Jonas way
+
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArr('Yo')('man');
+*/
+
+/*
+// The call and apply methods
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `{this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(423, 'William Chan');
+lufthansa.book(954, 'Matt Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does NOT work
+// book(23, 'Nick Suzuki');
+
+book.call(eurowings, 14, 'Nick Suzuki');
+console.log(eurowings);
+
+book.call(lufthansa, 253, 'Mary Poppins');
+
+const swiss = {
+  airline: 'Swiss Air lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 534, 'Mary Cooper');
+console.log(swiss);
+
+// Apply method
+const flightData = [583, 'Tommy Jeans'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+// apply no longer used, instead we use call with spread operator
+book.call(swiss, ...flightData);
+
+// The bind Method
+// book.call(eurowings, 23, 'Nick Suzuki');
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+bookEW(22, 'Cole Caufield');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Carey Price');
+bookEW23('Saku Koivu');
+
+// With eventListener
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+// lufthansa.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = (rate, value) => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const tax = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+console.log(tax(0.1)(200));
+
+const addVAT2 = tax(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
+*/
