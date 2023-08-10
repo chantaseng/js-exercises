83fd9f68049fc38c84ef80cdff19742ba52a7082
William Chan <chan.william.ts@gmail.com>
2023-08-10 16:43:30 -0400

    Really understnding the power of closure

--- 10-A-Closer-Look-at-Functions/Challenge-2/script.js
+++ 10-A-Closer-Look-at-Functions/Challenge-2/script.js
@@ -120,3 +120,36 @@ document
   .querySelector('.poll')
   .addEventListener('click', poll1.registerNewAnswer1.bind(poll1));
 */
+
+// Challenge #2
+
+/*
+Your tasks:
+
+1. Take the IIFE below and at the end of the function,attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the body element is clicked. Do not select the h1 element again!
+
+2. And now explain to yourself (or someone around you) why this worked! Take all the time you need. Think about when exactly the callback function is executed, and what that means for the variables involved in this example.
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+
+// header is in the backpack of the event function
+
+// Just for fun, everytime i click the body it changes color
+// (function () {
+//   const header = document.querySelector('h1');
+//   header.style.color = 'red';
+//   document.querySelector('body').addEventListener('click', function () {
+//     if (header.style.color === 'blue') {
+//       header.style.color = 'red';
+//     } else if ((header.style.color = 'red')) {
+//       header.style.color = 'blue';
+//     }
+//   });
+// })();
