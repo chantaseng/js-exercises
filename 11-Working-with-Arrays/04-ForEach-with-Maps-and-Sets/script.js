0585bef2cf5a2478103f547c3182c4b712b49b47
William Chan <chan.william.ts@gmail.com>
2023-08-11 16:51:04 -0400

    ForEach with sets and maps

--- 11-Working-with-Arrays/04-ForEach-with-Maps-and-Sets/script.js
+++ 11-Working-with-Arrays/04-ForEach-with-Maps-and-Sets/script.js
@@ -65,12 +65,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
@@ -153,3 +147,21 @@ movements.forEach(function (movement, i, arr) {
 // 2: function(400);
 // ...
 */
+
+// Map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Sets
+const currenciesUnique = new Set(['USD', 'GBP', 'EUR', 'GBP', 'USD']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
