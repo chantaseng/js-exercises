96c91c3a2541017e34066f7e9c023479dc83045f
William Chan <chan.william.ts@gmail.com>
2023-08-12 15:38:58 -0400

    Learned how to use the MAP method

--- 11-Working-with-Arrays/05-The-Map-Method/script.js
+++ 11-Working-with-Arrays/05-The-Map-Method/script.js
@@ -275,3 +275,33 @@ checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 console.log('-----------');
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 */
+
+// the map method
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUs = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUs;
+// });
+
+// Refactor into arrow function
+const movementsUSD = movements.map(mov => mov * eurToUs);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUs);
+
+console.log(movementsUSDfor);
+
+const movementsDescription = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+
+console.log(movementsDescription);
