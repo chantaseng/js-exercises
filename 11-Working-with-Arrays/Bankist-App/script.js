2e704ae8243e4c3373726ad75662de98dd102c86
William Chan <chan.william.ts@gmail.com>
2023-08-12 16:08:19 -0400

    Computing username: Used .map() to create new array for username(first letter of username) and used forEach() to create a new key

--- 11-Working-with-Arrays/Bankist-App/script.js
+++ 11-Working-with-Arrays/Bankist-App/script.js
@@ -6,14 +6,14 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'William Chan',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Khaliun Batmunkh',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
@@ -81,6 +81,18 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
