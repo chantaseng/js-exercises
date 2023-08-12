007a03d985f7c783c7f21b4d067e90a571800eb5
William Chan <chan.william.ts@gmail.com>
2023-08-12 16:21:08 -0400

    Learned another data transformation method

--- 11-Working-with-Arrays/06-The-Filter-Method/script.js
+++ 11-Working-with-Arrays/06-The-Filter-Method/script.js
@@ -98,7 +98,7 @@ createUsernames(accounts);
 /////////////////////////////////////////////////
 // LECTURES
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -202,3 +202,18 @@ currenciesUnique.forEach(function (value, _, map) {
 */
 
 // the filter method
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+
+console.log(withdrawals);
