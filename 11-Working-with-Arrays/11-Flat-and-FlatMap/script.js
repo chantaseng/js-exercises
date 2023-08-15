409f56a74ab7762cfbdfd93f62854abd47864a03
William Chan <chan.william.ts@gmail.com>
2023-08-15 18:26:31 -0400

    Introduction of flat and flatMap method. flatMap only goes one lvl deep for nested arrays, so thats why flat is still relevant

--- 11-Working-with-Arrays/11-Flat-and-FlatMap/script.js
+++ 11-Working-with-Arrays/11-Flat-and-FlatMap/script.js
@@ -474,6 +474,31 @@ console.log(movements.filter(deposit));
 
 // Flat and FlatMap
 
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// const accountMovements = accounts.map(acc => acc.movements);
+// console.log(accountMovements);
+// const allMovements = accountMovements.flat();
+// console.log(allMovements);
+// const overallBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
+
+// flat
+const overallBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance);
+
+// flatMap
+const overallBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance2);
+
 ////////////////////////////////////////////////
 // CHALLENGES
 /*
