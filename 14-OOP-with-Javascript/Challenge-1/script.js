893ae9534692823c5950143d775594ee5d82faf8
William Chan <chan.william.ts@gmail.com>
2023-09-08 17:07:57 -0400

    Challenge 1 completed - creation of constructor function and prototype method

--- 14-OOP-with-Javascript/Challenge-1/script.js
+++ 14-OOP-with-Javascript/Challenge-1/script.js
@@ -89,3 +89,29 @@ Test data:
 Data car 1: 'BMW' going at 120 km/h
 Data car 2: 'Mercedes' going at 95 km/h
 */
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+
+mercedes.accelerate();
+mercedes.brake();
+mercedes.brake();
