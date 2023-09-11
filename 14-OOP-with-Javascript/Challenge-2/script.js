bdb39bad28fe20ebef04b9eae940df9397ee35bf
William Chan <chan.william.ts@gmail.com>
2023-09-11 16:21:58 -0400

    Challenge 2 completed

--- 14-OOP-with-Javascript/Challenge-2/script.js
+++ 14-OOP-with-Javascript/Challenge-2/script.js
@@ -249,3 +249,36 @@ methods, and with the getter and setter.
 Test data:
 Data car 1: 'Ford' going at 120 km/h
 */
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed}km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed}km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new CarCl('Ford', 120);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford);
