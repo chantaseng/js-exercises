983fa7b02117f4131e440a2aba901275a4482b7e
William Chan <chan.william.ts@gmail.com>
2023-09-20 18:00:18 -0400

    babel and polyfilling

--- 17-Modern-Javascript-Dev-Tools/03-Module-Pattern/script.js
+++ 17-Modern-Javascript-Dev-Tools/03-Module-Pattern/script.js
@@ -1,10 +1,9 @@
-/*
 // Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // addToCart('bread', 5);
 // console.log(price, tq);
 
-console.log("Importing module");
+console.log('Importing module');
 // console.log(shippingCost);
 
 // import * as ShoppingCart from './shoppingCart.js';
@@ -14,10 +13,10 @@ console.log("Importing module");
 // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // console.log(price);
 
-import add, { cart } from "./shoppingCart.js";
-add("pizza", 2);
-add("bread", 5);
-add("apples", 4);
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
 
 console.log(cart);
 
@@ -27,22 +26,21 @@ console.log(cart);
 // console.log(data);
 // console.log("something");
 
-const getLastPost = async function () {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-  const data = await res.json();
+// const getLastPost = async function () {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+//   const data = await res.json();
 
-  return { title: data.at(-1).title, text: data.at(-1).body };
-};
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
 
-const lastPost = getLastPost();
-console.log(lastPost);
+// const lastPost = getLastPost();
+// console.log(lastPost);
 
 // Not very clean
 // lastPost.then((last) => console.log(last));
 
-const lastPost2 = await getLastPost();
-console.log(lastPost2);
-*/
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
 
 //////////////////////////////////////////////
 // The module pattern
